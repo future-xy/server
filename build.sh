eca28d22c56737182280dfc5639923d8d2413be9
Yao Fu <fuyao3860@gmail.com>
2023-06-04 20:35:47 +0100

    new triton container

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
+# # generate Dockerfile.compose (only run once)
+# python3 compose.py \
+# --backend pytorch \
+# --repoagent checksum \
+# --container-version 22.08 \
+# --dry-run
+
 # must provide a tag
 if [ $# -eq 0 ]
   then
@@ -17,19 +24,11 @@ cd ../server/
 cd ../pytorch_backend/
 bash build.sh
 cp -r -u ./build/install/backends/pytorch ../server/
+cp -u ./build/Phantom-component/phantom_checkpoint/libphantom_checkpoint.so ../server/pytorch/
 cd ../server/
 
 # copy scheduling.proto
 cp ~/Phantom-dev/GPU-serverless/pkg/scheduler/proto/scheduling.proto .
-
-# copy libs
-mkdir -p lib
-cp $HOME/.conda/envs/torch_dev/lib/libmkl_intel_lp64.so.1 ./lib/
-cp $HOME/.conda/envs/torch_dev/lib/libmkl_gnu_thread.so.1 ./lib/
-cp $HOME/.conda/envs/torch_dev/lib/libmkl_core.so.1 ./lib/
-cp /usr/lib/x86_64-linux-gnu/openmpi/lib/libmpi_cxx.so.40.20.1 ./lib/
-cp /lib/x86_64-linux-gnu/libmpi_cxx.so.40 ./lib/
-cp -r /usr/local/cuda/extras/CUPTI/lib64 ./lib/
 
 # /home/sl/.conda/envs/triton_dev/bin/python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. scheduling.proto
 docker build -t futurexy/tritonserver-sb:$TAG -f Dockerfile.compose .
